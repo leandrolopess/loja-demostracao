5d4862c068dcf97d3fe1d18f1962ba0f51ab5976
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-01-28 13:52:41 -0300

    Adicionado função para não permitir que usuarios que não sejam admin possam editar subcategorias

--- add/controller/subcategorias_editar.php
+++ add/controller/subcategorias_editar.php
@@ -1,6 +1,16 @@
 <?php 
  
 $smarty = new Template();
+
+if(Login::LogadoADM()){
+	$smarty->assign('USER', $_SESSION['ADM']['user_nome']);
+	$smarty->assign('DATA', $_SESSION['ADM']['user_data']);
+	$smarty->assign('HORA', $_SESSION['ADM']['user_hora']);
+	$smarty->assign('SOBRENOME', $_SESSION['ADM']['user_sobrenome']);
+	$smarty->assign('STATUS', $_SESSION['ADM']['user_status']);
+}
+
+
 $categorias = new Categorias();
 
 if(isset($_POST['cate_id']) && isset($_POST['cate_nome'])){
@@ -15,7 +25,7 @@ $smarty->assign('SUB_NOME', $sub_nome);
 $smarty->assign('SUB_ID', $sub_id);
 $smarty->assign('SUB_CATEGORIA', $sub_categoria);
 
-  if(isset($_POST['cate_editar'])){
+  if(isset($_POST['cate_editar']) && $_SESSION['ADM']['user_status'] == 'admin'){
      $subcategoria_id = $_POST['cate_id'];
      $subcategoria_nome = $_POST['cate_nome'];
      $subcategoria_categoria = $_POST['cate_sub'];
@@ -29,9 +39,6 @@ $smarty->assign('SUB_CATEGORIA', $sub_categoria);
      exit();
     }
   }
-
-
-
 
 
 $smarty->assign('GET_TEMA', Rotas::get_SiteTEMA());
