d19562c1fbd3a4211289f544f95232fe8bf11d38
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-01-29 20:05:17 -0300

    Arrumando o link do gif do arquivo cadastro.php da pasta controller

--- controller/cadastro.php
+++ controller/cadastro.php
@@ -70,8 +70,9 @@ if(isset($_POST['cli_nome']) and isset($_POST['cli_email']) and isset($_POST['cl
 
 
     // Mensagem ao cadastrar com sucesso
+    $site_tema = Config::GET_TEMA;
     echo '<div id="preloader">
-         <img class="preloader" src="http://localhost/loja/view/assets/images/gif/loading.gif" alt="">
+         <img class="preloader" src="'.$site_tema.'/assets/images/gif/loading.gif" alt="">
       </div>';
    Rotas::Redirecionar(1, Rotas::pag_ClienteConta());
 
